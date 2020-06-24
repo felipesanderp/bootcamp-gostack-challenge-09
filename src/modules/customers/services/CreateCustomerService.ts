a46546c1c12405cec859f3921b9a3029b35bad45
felipesanderp <felipe.panisson@hotmail.com>
2020-06-24 19:29:55 -0300

    add customers table

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const CheckUserExists = await this.customersRepository.findByEmail(email);
+
+    if (CheckUserExists) {
+      throw new AppError('This email already exists.');
+    }
+
+    const customer = this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
